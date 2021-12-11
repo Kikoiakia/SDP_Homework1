5a51298da527c25b3c56db29705bc63bc26721dc
Kiril Gourinov <kikoiakia@gmail.com>
2021-12-11 19:58:32 +0200

    Made better implementation

--- MrazMag/implementation.cpp
+++ MrazMag/implementation.cpp
@@ -8,22 +8,25 @@ const int RESTOCK_AMOUNT = 100;
 struct Worker {
 	int minuteSend;
 	ResourceType type;
-	int minuteBack;
 };
 struct MyStore : Store
 {
+private:
 	ActionHandler* actionHandler = nullptr;
 	std::vector<Client> clients;
 	std::vector<Worker> workers;
 	std::vector<int> indexes;
-	int incomingBananas = 0;
-	int incomingSchweppes = 0;
+	int stockBananas = 0;
+	int stockSchweppes = 0;
+	int localTime = 0;
+	int workerBananas = 0;
+	int workerSchweppes = 0;
 	int clientsCount = 0;
-	int workerCount = 0;
-	int bananas = 0;
-	int schweppes = 0;
-	int time = 0;
+	int availableWorkers = 0;
+	int bananaWorkers = 0;
+	
 
+public:
 	void setActionHandler(ActionHandler* handler) override
 	{
 		actionHandler = handler;
@@ -31,107 +34,49 @@ struct MyStore : Store
 
 	void init(int workerCount = 0, int startBanana = 0, int startSchweppes = 0) override
 	{
-		this->workerCount = workerCount;
-		bananas = startBanana;
-		schweppes = startSchweppes;
+		this->availableWorkers = workerCount;
+		stockBananas = startBanana;
+		stockSchweppes = startSchweppes;
 	}
 
 	void addClients(const Client* clients, int count) override
 	{
 		for (int i = 0; i < count; i++) {
-			this->clients.push_back(*(clients+i));
+			this->clients.push_back(clients[i]);
 		}
 		clientsCount += count;
 	}
 
 	void advanceTo(int minute) override
 	{
-		for (int i = 0; i < clientsCount; i++)
+		CheckInventory(minute);
+
+		RechargeStock(minute);
+
+		CheckDeparture(minute);
+
+		localTime = minute;
+	}
+
+	virtual int getBanana() const
+	{
+		return stockBananas;
+	}
+
+	virtual int getSchweppes() const
+	{
+		return stockSchweppes;
+	}
+
+
+
+	private:
+
+		void CheckInventory(int minute)
 		{
-			bool away = false;
-			for (int j = 0; j < indexes.size(); j++) {
-				if (i == indexes[j]) {
-					away = true;
-					break;
-				}
-			}
-			if (away) continue;
-			if (clients[i].arriveMinute <= minute && clients[i].arriveMinute >= time)
+
+			for (int i = 0; i < clientsCount; i++)
 			{
-
-				bool hasBananas = false;
-				bool hasSchweppes = false;
-				if (clients[i].banana > bananas + incomingBananas && workerCount > 0)
-				{
-					bool workerSend = false;
-					for (int j = 0; j < workers.size(); j++) {
-						if (workers[j].type == ResourceType::banana) {
-							workerSend = true;
-							break;
-						}
-					}
-					int stockWanted = clients[i].banana;
-					while (stockWanted - bananas > 0 && workerCount > 0 && !workerSend) {
-						actionHandler->onWorkerSend(minute, ResourceType::banana);
-						Worker worker = { clients[i].arriveMinute, ResourceType::banana, clients[i].arriveMinute + RESTOCK_TIME };
-						workers.push_back(worker);
-						workerCount--;
-						stockWanted -= RESTOCK_AMOUNT;
-						incomingBananas += RESTOCK_AMOUNT;
-					}
-				}
-				else if(clients[i].banana <= bananas)
-					hasBananas = true;
-
-				if (clients[i].schweppes > schweppes + incomingSchweppes && workerCount > 0)
-				{
-					bool workerSend = false;
-					for (int j = 0; j < workers.size(); j++) {
-						if (workers[j].type == ResourceType::schweppes) {
-							workerSend = true;
-							break;
-						}
-					}
-					int stockWanted = clients[i].schweppes;
-					while (stockWanted - schweppes > 0 && workerCount > 0 && !workerSend) {
-						actionHandler->onWorkerSend(minute, ResourceType::schweppes);
-						Worker worker = { clients[i].arriveMinute, ResourceType::schweppes, clients[i].arriveMinute + RESTOCK_TIME };
-						workers.push_back(worker);
-						workerCount--;
-						stockWanted -= RESTOCK_AMOUNT;
-						incomingSchweppes += RESTOCK_AMOUNT;
-					}
-				}
-				else if(clients[i].schweppes <= schweppes)
-					hasSchweppes = true;
-
-				if(hasBananas && hasSchweppes)
-				{
-					actionHandler->onClientDepart(i, minute, clients[i].banana, clients[i].schweppes);
-					bananas -= clients[i].banana;
-					schweppes -= clients[i].schweppes;
-					indexes.push_back(i);
-				}
-			}
-		}
-
-		bool recharged = false;
-		for (int i = 0; i < workers.size(); i++) {
-			if (workers[i].minuteBack <= minute && workers[i].minuteBack >= time) {
-				actionHandler->onWorkerBack(workers[i].minuteBack, workers[i].type);
-				if (workers[i].type == ResourceType::banana)
-					bananas += RESTOCK_AMOUNT;
-				else
-					schweppes += RESTOCK_AMOUNT;
-				workers.erase(workers.begin()+i);
-				workerCount++;
-				recharged = true;
-				i--;
-			}
-		}
-		if (recharged) {
-			for (int i = 0; i < clientsCount; i++) {
-
 				bool away = false;
 				for (int j = 0; j < indexes.size(); j++) {
 					if (i == indexes[j]) {
@@ -140,76 +85,162 @@ struct MyStore : Store
 					}
 				}
 				if (away) continue;
-				if (clients[i].arriveMinute <= minute){
-					if (clients[i].banana <= bananas && clients[i].schweppes <= schweppes) {
+				if (clients[i].arriveMinute <= minute && clients[i].arriveMinute >= localTime)
+				{
+
+					bool hasBananas = false;
+					bool hasSchweppes = false;
+					if (clients[i].banana <= stockBananas) hasBananas = true;
+					if (clients[i].schweppes <= stockSchweppes) hasSchweppes = true;
+
+					if (hasBananas && hasSchweppes)
+					{
 						actionHandler->onClientDepart(i, minute, clients[i].banana, clients[i].schweppes);
-						bananas -= clients[i].banana;
-						schweppes -= clients[i].schweppes;
+						stockBananas -= clients[i].banana;
+						stockSchweppes -= clients[i].schweppes;
 						indexes.push_back(i);
+					}
+					else if (clients[i].banana > stockBananas + workerBananas && clients[i].schweppes > stockSchweppes + workerSchweppes) {
+						int wantedBanana = clients[i].banana - workerBananas;
+						int wantedSchweppes = clients[i].schweppes - workerSchweppes;
+						while (wantedSchweppes >= 0 && wantedBanana >= 0) {
+							if (wantedBanana >= wantedSchweppes && availableWorkers > 0) {
+								actionHandler->onWorkerSend(minute, ResourceType::banana);
+								Worker worker = { clients[i].arriveMinute, ResourceType::banana };
+								workers.push_back(worker);
+								availableWorkers--;
+								wantedBanana -= RESTOCK_AMOUNT;
+								workerBananas += RESTOCK_AMOUNT;
+							}
+							else if(wantedBanana < wantedSchweppes && availableWorkers > 0){
+								actionHandler->onWorkerSend(minute, ResourceType::schweppes);
+								Worker worker = { clients[i].arriveMinute, ResourceType::schweppes };
+								workers.push_back(worker);
+								availableWorkers--;
+								wantedSchweppes -= RESTOCK_AMOUNT;
+								workerSchweppes += RESTOCK_AMOUNT;
+							}
+							else {
+								break;
+							}
+						}
+					}
+					else if (clients[i].banana > stockBananas + workerBananas) {
+						int wantedBanana = clients[i].banana - workerBananas;
+						while (wantedBanana > 0 && availableWorkers > 0) {
+							actionHandler->onWorkerSend(minute, ResourceType::banana);
+							Worker worker = { clients[i].arriveMinute, ResourceType::banana };
+							workers.push_back(worker);
+							availableWorkers--;
+							wantedBanana -= RESTOCK_AMOUNT;
+							workerBananas += RESTOCK_AMOUNT;
+						}
+					}
+					else {
+						int wantedSchweppes = clients[i].schweppes - workerSchweppes;
+						while (wantedSchweppes > 0 && availableWorkers > 0) {
+							actionHandler->onWorkerSend(minute, ResourceType::schweppes);
+							Worker worker = { clients[i].arriveMinute, ResourceType::schweppes };
+							workers.push_back(worker);
+							availableWorkers--;
+							wantedSchweppes -= RESTOCK_AMOUNT;
+							workerSchweppes += RESTOCK_AMOUNT;
+						}
 					}
 				}
 			}
 		}
-		for(int i = 0; i < clientsCount; i++){
 
-			bool away = false;
-			for (int j = 0; j < indexes.size(); j++) {
-				if (i == indexes[j])
-				away = true;
+		void RechargeStock(int minute)
+		{
+			bool recharged = false;
+			for (int i = 0; i < workers.size(); i++) {
+				if (workers[i].minuteSend + RESTOCK_TIME <= minute && workers[i].minuteSend + RESTOCK_TIME >= localTime) {
+					actionHandler->onWorkerBack(workers[i].minuteSend + RESTOCK_TIME, workers[i].type);
+					if (workers[i].type == ResourceType::banana)
+						stockBananas += RESTOCK_AMOUNT;
+					else
+						stockSchweppes += RESTOCK_AMOUNT;
+					workers.erase(workers.begin() + i);
+					availableWorkers++;
+					recharged = true;
+					i--;
+				}
 			}
-			if (away) continue;
-			if (clients[i].arriveMinute + clients[i].maxWaitTime <= minute && clients[i].arriveMinute + clients[i].maxWaitTime >= time)
-			{
-				if (clients[i].banana > bananas)
-				{
-					if (clients[i].schweppes > schweppes)
-					{
-						actionHandler->onClientDepart(i, minute, bananas, schweppes);
-						bananas = 0;
-						schweppes = 0;
-						indexes.push_back(i);
+			if (recharged) {
+				for (int i = 0; i < clientsCount; i++) {
+
+					bool away = false;
+					for (int j = 0; j < indexes.size(); j++) {
+						if (i == indexes[j]) {
+							away = true;
+							break;
+						}
 					}
-					else
-					{
-						actionHandler->onClientDepart(i, minute, bananas, clients[i].schweppes);
-						bananas -= 0;
-						schweppes -= clients[i].schweppes;
-						indexes.push_back(i);
-					}
-				}
-				else
-				{
-					if (clients[i].schweppes > schweppes)
-					{
-						actionHandler->onClientDepart(i, minute, clients[i].banana, schweppes);
-						bananas -= clients[i].banana;
-						schweppes = 0;
-						indexes.push_back(i);
-					}
-					else
-					{
-						actionHandler->onClientDepart(i, minute, clients[i].banana, clients[i].schweppes);
-						bananas -= clients[i].banana;
-						schweppes -= clients[i].schweppes;
-						indexes.push_back(i);
+					if (away) continue;
+					if (clients[i].arriveMinute <= minute) {
+						if (clients[i].banana <= stockBananas && clients[i].schweppes <= stockSchweppes) {
+							actionHandler->onClientDepart(i, minute, clients[i].banana, clients[i].schweppes);
+							stockBananas -= clients[i].banana;
+							stockSchweppes -= clients[i].schweppes;
+							indexes.push_back(i);
+						}
 					}
 				}
-				
 			}
 		}
-		
-		time = minute;
-	}
 
-	virtual int getBanana() const
-	{
-		return bananas;
-	}
 
-	virtual int getSchweppes() const
-	{
-		return schweppes;
-	}
+		void CheckDeparture(int minute)
+		{
+			for (int i = 0; i < clientsCount; i++) {
+
+				bool away = false;
+				for (int j = 0; j < indexes.size(); j++) {
+					if (i == indexes[j])
+						away = true;
+				}
+				if (away) continue;
+				if (clients[i].arriveMinute + clients[i].maxWaitTime <= minute && clients[i].arriveMinute + clients[i].maxWaitTime >= localTime)
+				{
+					if (clients[i].banana > stockBananas)
+					{
+						if (clients[i].schweppes > stockSchweppes)
+						{
+							actionHandler->onClientDepart(i, minute, stockBananas, stockSchweppes);
+							stockBananas = 0;
+							stockSchweppes = 0;
+							indexes.push_back(i);
+						}
+						else
+						{
+							actionHandler->onClientDepart(i, minute, stockBananas, clients[i].schweppes);
+							stockBananas = 0;
+							stockSchweppes -= clients[i].schweppes;
+							indexes.push_back(i);
+						}
+					}
+					else
+					{
+						if (clients[i].schweppes > stockSchweppes)
+						{
+							actionHandler->onClientDepart(i, minute, clients[i].banana, stockSchweppes);
+							stockBananas -= clients[i].banana;
+							stockSchweppes = 0;
+							indexes.push_back(i);
+						}
+						else
+						{
+							actionHandler->onClientDepart(i, minute, clients[i].banana, clients[i].schweppes);
+							stockBananas -= clients[i].banana;
+							stockSchweppes -= clients[i].schweppes;
+							indexes.push_back(i);
+						}
+					}
+
+				}
+			}
+		}
 };
 
 Store* createStore()
